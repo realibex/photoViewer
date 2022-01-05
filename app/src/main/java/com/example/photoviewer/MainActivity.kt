414861c8179db8314c8b47246f8ddc2b1e03a046
realibex <ibex97@naver.com>
2022-01-06 00:46:27 +0900

    멤버 별 클릭 추가

--- app/src/main/java/com/example/photoviewer/MainActivity.kt
+++ app/src/main/java/com/example/photoviewer/MainActivity.kt
@@ -16,9 +16,61 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this,"1번 클릭 완료", Toast.LENGTH_SHORT).show()
 
-            var intent = Intent(this,Img1Activity::class.java)
-            startActivity(intent)
+            var intent1 = Intent(this,Img1Activity::class.java)
+            startActivity(intent1)
+        }
 
+        val bts2 = findViewById<ImageView>(R.id.bts2)
+        bts2.setOnClickListener {
+
+            Toast.makeText(this,"2번 클릭 완료", Toast.LENGTH_SHORT).show()
+
+            var intent2 = Intent(this,Img2Activity::class.java)
+            startActivity(intent2)
+        }
+
+        val bts3 = findViewById<ImageView>(R.id.bts3)
+        bts3.setOnClickListener {
+
+            Toast.makeText(this,"3번 클릭 완료", Toast.LENGTH_SHORT).show()
+
+            var intent3 = Intent(this,Img3Activity::class.java)
+            startActivity(intent3)
+        }
+
+        val bts4 = findViewById<ImageView>(R.id.bts4)
+        bts4.setOnClickListener {
+
+            Toast.makeText(this,"4번 클릭 완료", Toast.LENGTH_SHORT).show()
+
+            var intent4 = Intent(this,Img4Activity::class.java)
+            startActivity(intent4)
+        }
+
+        val bts5 = findViewById<ImageView>(R.id.bts5)
+        bts5.setOnClickListener {
+
+            Toast.makeText(this,"5번 클릭 완료", Toast.LENGTH_SHORT).show()
+
+            var intent5 = Intent(this,Img5Activity::class.java)
+            startActivity(intent5)
+        }
+
+        val bts6 = findViewById<ImageView>(R.id.bts6)
+        bts6.setOnClickListener {
+
+            Toast.makeText(this,"6번 클릭 완료", Toast.LENGTH_SHORT).show()
+
+            var intent6 = Intent(this,Img6Activity::class.java)
+            startActivity(intent6)
+        }
+
+        val bts7 = findViewById<ImageView>(R.id.bts7)
+        bts7.setOnClickListener {
+            Toast.makeText(this,"7번 클릭완료",Toast.LENGTH_SHORT).show()
+
+            var intent7 = Intent(this,Img7Activity::class.java)
+            startActivity(intent7)
         }
 
     }
